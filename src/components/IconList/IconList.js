3c4b1e8cbaee1506ab2144ac028701a14f782157
Ishita Roy <ishitaroy@Ishitas-Macbook.local>
2021-06-02 00:55:14 +0530

    Changed icons, added css styles

--- src/components/IconList/IconList.js
+++ src/components/IconList/IconList.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import FileCopyRoundedIcon from '@material-ui/icons/FileCopyRounded';
-import LockRoundedIcon from '@material-ui/icons/LockRounded';
-import PermPhoneMsgRoundedIcon from '@material-ui/icons/PermPhoneMsgRounded';
 import { Typography } from '@material-ui/core';
 import './IconList.css';
+import file from '../../assets/file.png';
+import lock from '../../assets/lock.png';
+import support from '../../assets/support.png';
 
 function IconList() {
     return (
         <div className='icon-container'>
             <div className='row'>
-                <div className='col'><FileCopyRoundedIcon color="secondary"  style={{ fontSize: 40}}/></div>
+                <div className='col'><img src={file} alt='file folder icon' style={{width: '40px', padding: '5px'}}/></div>
                 <div className='col'><Typography variant='h6'>Create and save unlimited legal documents</Typography></div>
             </div>
             <div className='row'>
-                <div className='col'><LockRoundedIcon color="secondary"  style={{ fontSize: 40}}/></div>
+                <div className='col'><img src={lock} alt='lock folder icon' style={{width: '50px'}}/></div>
                 <div className='col'><Typography variant='h6'>Never lose your work</Typography></div>
             </div>
             <div className='row'>
-                <div className='col'><PermPhoneMsgRoundedIcon color="secondary"  style={{ fontSize: 40}}/></div>
+                <div className='col'><img src={support} alt='file folder icon' style={{width: '50px'}}/></div>
                 <div className='col'><Typography variant='h6'>Free Technical support</Typography></div>
             </div>
         </div>
