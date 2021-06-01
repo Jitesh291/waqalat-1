7603108b341f349d631ad6994efeabab4e95ca6c
Ishita Roy <ishitaroy@Ishitas-Macbook.local>
2021-06-01 12:06:52 +0530

    Footer formatting

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,11 @@ import './Footer.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Link } from 'react-router-dom';
 class Footer extends Component {
     render() {
         return (
-            
+            <React.Fragment>
             <div className='footer-container'>
                 <div className='footer-logos'>
                     <img src='./Waqalat_White.png' className='company-logo' alt='comapny logo'></img>
@@ -17,15 +18,22 @@ class Footer extends Component {
                     </div>
                 </div>
                 <div className='footer-nav-links'>
-                    2
+                    <Link to='/' className='footer-link'>Home</Link>
+                    <Link to='/' className='footer-link'>About</Link>
+                    <Link to='/' className='footer-link'>Contact</Link>
+                    <Link to='/' className='footer-link'>Help</Link>
                 </div>
                 <div className='footer-program-links'>
-                    3
+                    
                 </div>
                 <div className='footer-country-stickers'>
-                    4
+                    
                 </div>
             </div>
+            <div className='footer-tc'>
+                Your use of this site is subject to our Terms of Use, Disclaimer, Privacy Policy. LawDepot® is not a law firm and cannot provide legal advice. We provide information and software and you are responsible for appropriately using this material. ©2002-2021 LawDepot® (Sequiter Inc.). All Rights Reserved.
+            </div>
+            </React.Fragment>
         );
     }
 }
